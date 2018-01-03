96aac23937f9dd5c1f2fccff82b69aeedd853e6e
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 16:17:23 +0100

    hopefully this should work

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -1,25 +1,10 @@
-#!/bin/bash
-set -eo pipefail
-
-function main () {
-    export DEBIAN_FRONTEND=noninteractive
-    
-    apt-get update
-
-    apt-get install -y -q python3-dev python3-venv
-
-    mkdir ~/.venvs
-    python3 -m venv ~/.venvs/chaostk
-    source ~/.venvs/chaostk/bin/activate
-
-    pip install -q -U chaostoolkit
-
-    pip install -q -U chaostoolkit-kubernetes
-
-    git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
-
-    cd chaostoolkit-samples/service-down-not-visible-to-users
-}
-
-main "$@" || exit 1
-exit 0
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y -q python3-dev python3-venv
+mkdir ~/.venvs
+python3 -m venv ~/.venvs/chaostk
+source ~/.venvs/chaostk/bin/activate
+pip install -q -U chaostoolkit
+pip install -q -U chaostoolkit-kubernetes
+git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
+cd chaostoolkit-samples/service-down-not-visible-to-users
