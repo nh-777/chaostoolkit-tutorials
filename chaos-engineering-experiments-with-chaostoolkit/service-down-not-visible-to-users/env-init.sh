1f3e95ef4ae39e7f29feb751a1e9e6c788479d3d
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 16:32:56 +0100

    not ssh'ing

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -1,10 +1,10 @@
-ssh root@host01 "export DEBIAN_FRONTEND=noninteractive && \
-apt-get update && \
-apt-get install -y -q python3-dev python3-venv && \
-mkdir ~/.venvs && \
-python3 -m venv ~/.venvs/chaostk && \
-source ~/.venvs/chaostk/bin/activate && \
-pip install -q -U chaostoolkit && \
-pip install -q -U chaostoolkit-kubernetes && \
-git clone https://github.com/chaostoolkit/chaostoolkit-samples.git && \
-cd chaostoolkit-samples/service-down-not-visible-to-users"
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y -q python3-dev python3-venv
+mkdir ~/.venvs
+python3 -m venv ~/.venvs/chaostk
+source ~/.venvs/chaostk/bin/activate
+pip install -q -U chaostoolkit
+pip install -q -U chaostoolkit-kubernetes
+git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git
+cd chaostoolkit-samples/service-down-not-visible-to-users
