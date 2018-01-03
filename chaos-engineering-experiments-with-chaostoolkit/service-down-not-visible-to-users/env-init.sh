c06fa8f36a87c639a5f3485da8ec772ea88d202b
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 15:52:09 +0100

    moving to the background

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -2,27 +2,29 @@
 set -eo pipefail
 
 function main () {
-    echo "Updating package database"
-    apt-get update
+    ssh root@host01 <<EOF
+echo "Updating package database"
+apt-get update
 
-    echo "Installing system-wide Python dependencies"
-    apt-get install -y -q python3-dev python3-venv
+echo "Installing system-wide Python dependencies"
+apt-get install -y -q python3-dev python3-venv
 
-    echo "Preparing Python environment"
-    mkdir ~/.venvs
-    python3 -m venv ~/.venvs/chaostk
-    source ~/.venvs/chaostk/bin/activate
+echo "Preparing Python environment"
+mkdir ~/.venvs
+python3 -m venv ~/.venvs/chaostk
+source ~/.venvs/chaostk/bin/activate
 
-    echo "Install the chaostoolkit command"
-    pip install -q -U chaostoolkit
+echo "Install the chaostoolkit command"
+pip install -q -U chaostoolkit
 
-    echo "Install the chaostoolkit dependencies"
-    pip install -q -U chaostoolkit-kubernetes
+echo "Install the chaostoolkit dependencies"
+pip install -q -U chaostoolkit-kubernetes
 
-    echo "Downloading sample files"
-    git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
+echo "Downloading sample files"
+git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
 
-    cd chaostoolkit-samples/service-down-not-visible-to-users
+cd chaostoolkit-samples/service-down-not-visible-to-users
+EOF
 }
 
 main "$@" || exit 1
