05146b3a9d4d6fd2ac8b9b2b2696edf62db8d4eb
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 16:04:37 +0100

    ssh'ing doesn't seem required on kubernetes images

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -2,29 +2,27 @@
 set -eo pipefail
 
 function main () {
-    ssh root@host01 <<EOF
-echo "Updating package database"
-apt-get update
+    echo "Updating package database"
+    apt-get update
 
-echo "Installing system-wide Python dependencies"
-apt-get install -y -q python3-dev python3-venv
+    echo "Installing system-wide Python dependencies"
+    apt-get install -y -q python3-dev python3-venv
 
-echo "Preparing Python environment"
-mkdir ~/.venvs
-python3 -m venv ~/.venvs/chaostk
-source ~/.venvs/chaostk/bin/activate
+    echo "Preparing Python environment"
+    mkdir ~/.venvs
+    python3 -m venv ~/.venvs/chaostk
+    source ~/.venvs/chaostk/bin/activate
 
-echo "Install the chaostoolkit command"
-pip install -q -U chaostoolkit
+    echo "Install the chaostoolkit command"
+    pip install -q -U chaostoolkit
 
-echo "Install the chaostoolkit dependencies"
-pip install -q -U chaostoolkit-kubernetes
+    echo "Install the chaostoolkit dependencies"
+    pip install -q -U chaostoolkit-kubernetes
 
-echo "Downloading sample files"
-git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
+    echo "Downloading sample files"
+    git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
 
-cd chaostoolkit-samples/service-down-not-visible-to-users
-EOF
+    cd chaostoolkit-samples/service-down-not-visible-to-users
 }
 
 main "$@" || exit 1
