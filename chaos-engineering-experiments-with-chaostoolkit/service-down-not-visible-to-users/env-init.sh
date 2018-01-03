6593face4d707c1debc14722a30773d4b3a7798a
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 16:22:32 +0100

    not helping with multiple statements

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -1,12 +1,10 @@
-ssh root@host01 <<EOF
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y -q python3-dev python3-venv
-mkdir ~/.venvs
-python3 -m venv ~/.venvs/chaostk
-source ~/.venvs/chaostk/bin/activate
-pip install -q -U chaostoolkit
-pip install -q -U chaostoolkit-kubernetes
-git clone https://github.com/chaostoolkit/chaostoolkit-samples.git
-cd chaostoolkit-samples/service-down-not-visible-to-users
-EOF
+ssh root@host01 "export DEBIAN_FRONTEND=noninteractive && \
+apt-get update && \
+apt-get install -y -q python3-dev python3-venv && \
+mkdir ~/.venvs && \
+python3 -m venv ~/.venvs/chaostk && \
+source ~/.venvs/chaostk/bin/activate && \
+pip install -q -U chaostoolkit && \
+pip install -q -U chaostoolkit-kubernetes && \
+git clone https://github.com/chaostoolkit/chaostoolkit-samples.git && \
+cd chaostoolkit-samples/service-down-not-visible-to-users"
