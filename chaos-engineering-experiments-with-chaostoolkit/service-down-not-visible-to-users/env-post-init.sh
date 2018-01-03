15a2201147509b17202569489ac86d08e793281b
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 23:07:57 +0100

    front

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
@@ -1,3 +1,4 @@
 launch.sh
 source ~/.venvs/chaostk/bin/activate
+git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git
 cd chaostoolkit-samples/service-down-not-visible-to-users
