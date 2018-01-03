47657650ce13f69e5e4344b6d84f6083768777e5
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 16:49:37 +0100

    try again

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -1,1 +1,1 @@
-git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git
+ssh root@host01 "git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git && export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y -q python3-dev python3-venv && mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit && pip install -q -U chaostoolkit-kubernetes && cd chaostoolkit-samples/service-down-not-visible-to-users"
