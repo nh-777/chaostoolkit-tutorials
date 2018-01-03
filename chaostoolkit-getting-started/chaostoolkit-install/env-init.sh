396311dfbba7c26c558cf7265abbf009c47c16af
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 14:39:01 +0100

    installing in the foreground for now

--- chaostoolkit-getting-started/chaostoolkit-install/env-init.sh
+++ chaostoolkit-getting-started/chaostoolkit-install/env-init.sh
@@ -6,7 +6,7 @@ function main () {
     apt-get update
 
     echo "Installing system-wide Python dependencies"
-    apt-get install -y python3-dev python3-venv
+    apt-get install -y -q python3-dev python3-venv
 }
 
 main "$@" || exit 1
