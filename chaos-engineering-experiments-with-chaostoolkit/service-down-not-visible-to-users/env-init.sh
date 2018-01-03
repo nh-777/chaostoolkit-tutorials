9cfc058e5f253d70fbc90eafe2c64d1c7fc85ffb
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 22:45:22 +0100

    might actually be good now

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -1,5 +1,1 @@
-#ssh root@host01 "export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y python3-dev python3-venv"
-
-#&& mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit chaostoolkit-kubernetes && git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git && cd chaostoolkit-samples/service-down-not-visible-to-users"
-
-ssh root@host01 "curl -O https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz && tar zxvf virtualenv-15.1.0.tar.gz && cd virtualenv-15.1.0 && mkdir ~/.venvs && python3 virtualenv.py ~/.venvs/chaostk"
+ssh root@host01 "export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y python3-dev python3-venv && mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit chaostoolkit-kubernetes && git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git && cd chaostoolkit-samples/service-down-not-visible-to-users"
--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
@@ -2,8 +2,6 @@
 set -eo pipefail
 
 function main () {
-    curl -O https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz && tar zxvf virtualenv-15.1.0.tar.gz && cd virtualenv-15.1.0 && mkdir ~/.venvs && python3 virtualenv.py ~/.venvs/chaostk
-
     echo "Cloning the application and experiment assets"
     git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git
     cd chaostoolkit-samples/service-down-not-visible-to-users
@@ -12,7 +10,7 @@ function main () {
     launch.sh
 
     echo "Preparing the Python environment"
-    #mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk
+    mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk
     source ~/.venvs/chaostk/bin/activate
 
     echo "Installing chaostoolkit"
