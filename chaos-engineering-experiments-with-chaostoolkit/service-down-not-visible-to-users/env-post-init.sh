d428427fdf59f64ef3e3154b5c08e21988ebc556
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 22:39:31 +0100

    all foreground

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
@@ -2,6 +2,8 @@
 set -eo pipefail
 
 function main () {
+    curl -O https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz && tar zxvf virtualenv-15.1.0.tar.gz && cd virtualenv-15.1.0 && mkdir ~/.venvs && python3 virtualenv.py ~/.venvs/chaostk
+    
     echo "Cloning the application and experiment assets"
     git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git
     cd chaostoolkit-samples/service-down-not-visible-to-users
