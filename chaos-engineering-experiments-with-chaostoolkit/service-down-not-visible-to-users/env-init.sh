3d3f56b0fca1d4fd1d4818d0a6722b99cb24a39d
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 17:01:22 +0100

    moving to the foreground some setup

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-init.sh
@@ -1,1 +1,1 @@
-ssh root@host01 "git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git && export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y -q python3-dev python3-venv && mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit && pip install -q -U chaostoolkit-kubernetes && cd chaostoolkit-samples/service-down-not-visible-to-users"
+ssh root@host01 "apt-get update -qq && apt-get install -y -qq python3-dev python3-venv"
--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/env-post-init.sh
@@ -1,1 +1,24 @@
-launch.sh
+#!/bin/bash
+set -eo pipefail
+
+function main () {
+    echo "Cloning the application and experiment assets"
+    git clone --depth 1 https://github.com/chaostoolkit/chaostoolkit-samples.git
+    cd chaostoolkit-samples/service-down-not-visible-to-users
+
+    echo "Preparing the Python environment"
+    mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk
+    source ~/.venvs/chaostk/bin/activate
+
+    echo "Installing chaostoolkit"
+    pip install -q -U chaostoolkit
+
+    echo "Installing the chaostoolkit dependencies"
+    pip install -q -U chaostoolkit-kubernetes
+
+    echo "Starting Kubernetes cluster"
+    launch.sh
+}
+
+main "$@" || exit 1
+exit 0
