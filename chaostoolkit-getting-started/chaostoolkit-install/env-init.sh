fc07ece1454249e7df2d0fea04256b29f84943ab
Sylvain Hellegouarch <sh@defuze.org>
2018-01-03 14:07:30 +0100

    cleanups

--- chaostoolkit-getting-started/chaostoolkit-install/env-init.sh
+++ chaostoolkit-getting-started/chaostoolkit-install/env-init.sh
@@ -1,2 +1,10 @@
-apt-get update
-apt-get install -y python3-venv
+#!/bin/bash
+set -eo pipefail
+
+function main () {
+    apt-get update
+    apt-get install -y python3-dev python3-venv
+}
+
+main "$@" || exit 1
+exit 0
