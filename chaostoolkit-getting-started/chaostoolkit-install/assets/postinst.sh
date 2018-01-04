8cc5ee476db1aef687e36577f72160371e1d342c
Sylvain Hellegouarch <sh@defuze.org>
2018-01-04 12:02:35 +0100

    status of installed packages

--- chaostoolkit-getting-started/chaostoolkit-install/assets/postinst.sh
+++ chaostoolkit-getting-started/chaostoolkit-install/assets/postinst.sh
@@ -4,7 +4,7 @@ function main () {
     echo "Waiting for environment to be fully updated..."
     for i in {1..70}
     do
-        if dpkg -l python3-venv > /dev/null 2>&1; then
+        if dpkg -s python3-venv > /dev/null 2>&1; then
             break
         fi
         sleep 1s
@@ -12,7 +12,7 @@ function main () {
 
     # when we didn't break from the previous loop, it means the package
     # hasn't been installed yet, let's bail.
-    if ! dpkg -l python3-venv > /dev/null 2>&1; then
+    if ! dpkg -s python3-venv > /dev/null 2>&1; then
         echo "Environment took too long to be prepared. "\
             "Sorry, but you will have to refresh this page and try again :/"
         return 1
