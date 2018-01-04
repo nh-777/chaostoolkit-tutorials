44080c078f006c3c9ff44d59473be71675904b73
Sylvain Hellegouarch <sh@defuze.org>
2018-01-04 11:55:41 +0100

    cleanups

--- chaostoolkit-getting-started/chaostoolkit-install/env-init.sh
+++ chaostoolkit-getting-started/chaostoolkit-install/env-init.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-set -eo pipefail
-
-function main () {
-    echo "Updating package database"
-    apt-get update
-
-    echo "Installing system-wide Python dependencies"
-    apt-get install -y -q python3-dev python3-venv
-}
-
-main "$@" || exit 1
-exit 0
+ssh root@host01 "export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y python3-dev python3-venv"
