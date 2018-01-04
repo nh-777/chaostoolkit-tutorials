ac28f8cb887b292743d2867de4392992e2c93e45
Sylvain Hellegouarch <sh@defuze.org>
2018-01-04 12:13:28 +0100

    missing closing tag

--- chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/assets/get-consumer-service-address.sh
+++ chaos-engineering-experiments-with-chaostoolkit/service-down-not-visible-to-users/assets/get-consumer-service-address.sh
@@ -2,10 +2,9 @@
 
 function main () {
     local nodeIp=`kubectl get nodes --selector=kubernetes.io/role!=master -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address}`
-    local nodePort=`kubectl get svc my-consumer-service -o jsonpath={.spec.ports[0].nodePort`
+    local nodePort=`kubectl get svc my-consumer-service -o jsonpath={.spec.ports[0].nodePort}`
 
     export CONSUMER_ADDR=http://${nodeIp}:${nodePort}/
-
     echo "${CONSUMER_ADDR}"
 }
 
