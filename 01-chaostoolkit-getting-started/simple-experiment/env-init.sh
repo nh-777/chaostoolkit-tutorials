5ef82a5ef982c0f9ef6084435e8109641743334a
Sylvain Hellegouarch <sh@defuze.org>
2018-01-07 18:45:54 +0100

    get it first

--- 01-chaostoolkit-getting-started/simple-experiment/env-init.sh
+++ 01-chaostoolkit-getting-started/simple-experiment/env-init.sh
@@ -1,2 +1,2 @@
-mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit
 echo 'export PS1="\[\033[38;5;226m\]\t\[\] \[\033[01;34m\]\w\[\033[00m\]\$ "' >> $HOME/.bashrc
+mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit
