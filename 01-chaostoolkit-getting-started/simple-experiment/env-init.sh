6ea7cbad4895dd9a5ae2d8f786b8c45a32bdc8c7
Sylvain Hellegouarch <sh@defuze.org>
2018-01-07 18:48:39 +0100

    need at startup

--- 01-chaostoolkit-getting-started/simple-experiment/env-init.sh
+++ 01-chaostoolkit-getting-started/simple-experiment/env-init.sh
@@ -1,4 +1,4 @@
 echo 'export PS1="\[\033[38;5;226m\]\t\[\] \[\033[01;34m\]\w\[\033[00m\]\$ "' >> $HOME/.bashrc
-echo "export LC_ALL=C.UTF-8"
-echo "export LANG=C.UTF-8"
+echo "export LC_ALL=C.UTF-8" >> $HOME/.bashrc
+echo "export LANG=C.UTF-8" >> $HOME/.bashrc
 mkdir ~/.venvs && python3 -m venv ~/.venvs/chaostk && source ~/.venvs/chaostk/bin/activate && pip install -q -U chaostoolkit
